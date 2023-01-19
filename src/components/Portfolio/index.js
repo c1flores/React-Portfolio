6d26f4b23afaf10e1e14d063e72ff7b21dc2a58f
Christian Flores <c1flores@ucsd.edu>
2023-01-19 10:57:57 -0800

    Finished portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -7,20 +7,38 @@ function Portfolio() {
     {
       name: "Deja-Brew",
       description: "MERN Stack",
-      link: "https://github.com",
+      link: "Soon-To-Be-Posted-On-Heroku",
       repo: "https://github.com/c1flores/Deja-Brew",
     },
     {
       name: "Dinner-Spinner",
-      description: "JavaScript/Node/Express/SQL/RESTful API",
+      description: "JavaScript/Node/Express/SQL/RESTful-API",
       link: "https://dinner-spinnerrr.herokuapp.com/",
       repo: "https://github.com/c1flores/Dinner-Spinner",
     },
     {
+      name: "Tech-Blog",
+      description: "JavaScript/Node/Express/SQL/Handle-bars/HTML/CSS",
+      link: "https://techh-bloggg.herokuapp.com/",
+      repo: "https://github.com/c1flores/Tech-Blog",
+    },
+    {
+      name: "Weather-Dashboard",
+      description: "JavaScript/jQuery/HTML/CSS/Server-Side API/Web API",
+      link: "https://c1flores.github.io/Weather-Dashboard/",
+      repo: "https://github.com/c1flores/Weather-Dashboard",
+    },
+    {
       name: "Bottoms-Up",
-      description: "HTML/CSS/Bootstrap/Server-Side API",
+      description: "JavaScript/HTML/CSS/Bootstrap/Server-Side API",
       link: "https://leinadzz.github.io/Bottoms-Up-API/",
       repo: "https://github.com/c1flores/Bottoms-Up-API",
+    },
+    {
+      name: "Coding-Assessment",
+      description: "JavaScript/HTML/CSS/Local-Storage API",
+      link: "https://c1flores.github.io/Coding-Assessment/",
+      repo: "https://github.com/c1flores/Coding-Assessment",
     },
   ]);
 
